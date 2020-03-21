b4d5dde1b99043cb8620c7bbe0912a5147363bc0
Ben Hammel <bdhammel@gmail.com>
2020-03-20 21:47:57 -0700

    Include example file

--- ellipse.py
+++ ellipse.py
@@ -221,28 +221,3 @@ class LsqEllipse:
              + height * np.sin(t) * np.cos(phi))
 
         return np.c_[x, y]
-
-
-def make_test_ellipse(center=[1, 1], width=1, height=.6, phi=3.14/5):
-    """Generate Elliptical data with noise
-
-    Args
-    ----
-    center (list:float): (<x_location>, <y_location>)
-    width (float): semimajor axis. Horizontal dimension of the ellipse (**)
-    height (float): semiminor axis. Vertical dimension of the ellipse (**)
-    phi (float:radians): tilt of the ellipse, the angle the semimajor axis
-        makes with the x-axis
-
-    Returns
-    -------
-    data (list:list:float): list of two lists containing the x and y data of the
-        ellipse. of the form [[x1, x2, ..., xi],[y1, y2, ..., yi]]
-    """
-    t = np.linspace(0, 2*np.pi, 1000)
-    x_noise, y_noise = np.random.rand(2, len(t))
-
-    ellipse_x = center[0] + width*np.cos(t)*np.cos(phi)-height*np.sin(t)*np.sin(phi) + x_noise/2.
-    ellipse_y = center[1] + width*np.cos(t)*np.sin(phi)+height*np.sin(t)*np.cos(phi) + y_noise/2.
-
-    return [ellipse_x, ellipse_y]
